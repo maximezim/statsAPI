6bdade0b853fc672755f746a4574be2eb849f54b
maximezim <maxime.zimmermann57@gmail.com>
2024-12-05 14:57:22 +0100

    Add CORS middleware to allow cross-origin requests

--- app.py
+++ app.py
@@ -21,11 +21,23 @@ from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 redis_client = None
 admin_credentials = {}  
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
